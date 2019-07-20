2b99eae609fb6a438c5094f04abcc072109c42b0
shubham <shubham@piirs.in>
2019-07-20 17:15:06 +0530

    added get/post/put/patch/delete

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework import status
+from profiles_api import serializers
 # Create your views here.
 
 class HelloApiView(APIView):
+
+    serializer_class = serializers.HelloSerializer
 
     def get(self,request,formt=None):
         an_apiview = [
@@ -14,3 +17,24 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message':'Hello!','an_apiview':an_apiview})
+
+
+    def post(self,request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message':message})
+
+        else:
+            return Response(serializer.errors,status.HTTP_400_BAD_REQUEST)
+
+    def put(self,request,pk=None):
+        return Response({'method':'PUT'})
+
+    def patch(self,request,pk=None):
+        return Response({'method':'patch'})
+
+    def delete(self,request,pk=None):
+        return Response({'method':'delete'})
