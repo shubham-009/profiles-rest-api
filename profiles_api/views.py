0372943d2ea9822ec7bad5f2b67a2e9b003db374
shubham <shubham@piirs.in>
2019-07-20 16:52:57 +0530

    apiview is created

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+class HelloApiView(APIView):
+
+    def get(self,request,formt=None):
+        an_apiview = [
+               'Uses Http method as function (get,post,patch,put,delete)',
+               'Is similar to traditional django view',
+               'gives you the most control over ypur logic',
+               'IS mapped manually to urls',
+        ]
+
+        return Response({'message':'Hello!','an_apiview':an_apiview})
